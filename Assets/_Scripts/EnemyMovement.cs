eedbcacb8a01c93ab3d45b539ddea052f2595d2b
sharon moon <jsmoon883@gmail.com>
2023-01-11 21:28:13 -0600

    ..

--- Assets/_Scripts/EnemyMovement.cs
+++ Assets/_Scripts/EnemyMovement.cs
@@ -1,42 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class EnemyMovement : MonoBehaviour
-{   
-    public float speed;
-    private Vector3 direction;
-    private GameObject playObj=null;
-    private float player_x, player_y, distance, aggroRadius=10;
+// public class EnemyMovement : MonoBehaviour
+// {   
+//     public float speed;
+//     private Vector3 direction;
+//     private GameObject playObj=null;
+//     private float player_x, player_y, distance, aggroRadius=10;
 
 
 
-    // Start is called before the first frame update
-    //void Start()
-    //{    }
+//     // Start is called before the first frame update
+//     //void Start()
+//     //{    }
 
-    // Update is called once per frame
-    void Update(){
-        if (isClose){
-            Debug.Log("MADMADMADMAD");
-        }
+//     // Update is called once per frame
+//     void Update(){
+//         if (isClose){
+//             Debug.Log("MADMADMADMAD");
+//         }
         
-    }
+//     }
 
-    private bool isClose(){
+//     private bool isClose(){
 
-        //finding the player by name 
-        if (playerObj==null){
-            playerObj=GameObject.Find("Player");
-        } 
-        player_x=playerObj.transform.position.x;
-        player_y=playerObj.transform.position.y;
+//         //finding the player by name 
+//         if (playerObj==null){
+//             playerObj=GameObject.Find("Player");
+//         } 
+//         player_x=playerObj.transform.position.x;
+//         player_y=playerObj.transform.position.y;
 
-        // distance btwn player and enemy;
-        //there has to be a better way to do this...
-        distance=sqrt((player_x-transform.position.x)**2 +(player_y-tranform.position.y)**2);
-        if (distance <=aggroRadius){
-            return true;
-        }
-    }
-}
+//         // distance btwn player and enemy;
+//         //there has to be a better way to do this...
+//         distance=sqrt((player_x-transform.position.x)**2 +(player_y-tranform.position.y)**2);
+//         if (distance <=aggroRadius){
+//             return true;
+//         }
+//     }
+// }
--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -1,10 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public 
+    public Rigidbody2D rb;
+        
+    private PlayerControls playerControls;
+    private Vector2 move;
+    private float speed = 1f;
+
+    private void Awake() {
+        playerControls = new PlayerControls();
+    }
+
+    private void OnEnable() {
+        playerControls.Enable();
+    }
+
+    private void OnDisable() {
+        playerControls.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +29,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        move = playerControls.Player.Move.ReadValue<Vector2>().normalized; //gets the input from WASD and puts them into a vector
+        Debug.Log(move);
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + move * speed); //moves the player
     }
 }
